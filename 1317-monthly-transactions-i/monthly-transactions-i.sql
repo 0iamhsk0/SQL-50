489f0de93b6895c911f1fd4d7611f3fa73fbcd24
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-01 19:54:42 +0530

    Time: 440 ms (56.97%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -7,5 +7,5 @@ SELECT
     SUM(COALESCE(amount, 0)) AS trans_total_amount,
     SUM(CASE WHEN state = 'approved' THEN COALESCE(amount, 0) ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY month, country;
+GROUP BY 1, 2;
 
