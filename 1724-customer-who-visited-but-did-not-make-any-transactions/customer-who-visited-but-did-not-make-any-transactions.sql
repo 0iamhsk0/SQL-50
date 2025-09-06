894c787f3d5b073818fb8e8de882025c18747bea
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-06 17:38:51 +0530

    Time: 478 ms (76.27%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,13 +1,13 @@
-SELECT DISTINCT v.customer_id, COUNT(v.customer_id) count_no_trans
-FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id
-WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id;
+-- SELECT DISTINCT v.customer_id, COUNT(v.customer_id) count_no_trans
+-- FROM Visits v
+-- LEFT JOIN Transactions t
+-- ON v.visit_id = t.visit_id
+-- WHERE t.transaction_id IS NULL
+-- GROUP BY v.customer_id;
 
 -- OR
 
--- SELECT v.customer_id, count(v.customer_id) count_no_trans
--- FROM visits v
--- WHERE NOT EXISTS (SELECT 1 FROM transactions t WHERE v.visit_id = t.visit_id)
--- GROUP BY v.customer_id;
+SELECT v.customer_id, count(v.customer_id) count_no_trans
+FROM visits v
+WHERE NOT EXISTS (SELECT 1 FROM transactions t WHERE v.visit_id = t.visit_id)
+GROUP BY v.customer_id;
