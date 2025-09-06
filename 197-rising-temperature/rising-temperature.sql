7959b4888f96a0c8cedb857a932b0ae0c3eab0cf
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-06 18:44:58 +0530

    Time: 210 ms (94.89%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,10 +1,25 @@
 -- Write your PostgreSQL query statement below
 
 -- Using self join
-SELECT w1.id
-FROM Weather w1
-INNER JOIN Weather w2
-ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
-WHERE w1.temperature > w2.temperature;
+-- SELECT w1.id
+-- FROM Weather w1
+-- INNER JOIN Weather w2
+-- ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+-- WHERE w1.temperature > w2.temperature;
 
 -- Using lag
+WITH rising AS (
+  SELECT id,
+         recordDate,
+         temperature,
+         LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+         LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp
+    FROM Weather
+)
+SELECT id
+FROM rising
+WHERE recordDate = prev_date + INTERVAL '1 day'
+  AND temperature > prev_temp;
+
+
+
