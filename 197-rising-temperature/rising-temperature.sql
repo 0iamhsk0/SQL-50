56f62433e462ff65859f2cbbdfcef953ccb32638
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-06 18:25:57 +0530

    Time: 221 ms (77.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+
+-- Using self join
+SELECT w1.id
+FROM Weather w1
+INNER JOIN Weather w2
+ON w1.recordDate = w2.recordDate + INTERVAL '1 day'
+WHERE w1.temperature > w2.temperature;
+
+-- Using lag
