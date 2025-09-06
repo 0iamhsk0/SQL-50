3d7f89b0f74368aae2df5ee2bbfd631be8aeb412
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-06 20:24:56 +0530

    Time: 193 ms (30.09%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,15 @@
-SELECT b.machine_id, round(avg(b.dif)::numeric,3) processing_time
-FROM (SELECT a.machine_id, max(a.timestamp) - min(a.timestamp) dif
-    FROM activity a
-    GROUP BY a.machine_id, a.process_id) b
-GROUP BY b.machine_id;
+-- Subquery
+-- SELECT b.machine_id, round(avg(b.dif)::numeric,3) processing_time
+-- FROM (SELECT a.machine_id, max(a.timestamp) - min(a.timestamp) dif
+--     FROM activity a
+--     GROUP BY a.machine_id, a.process_id) b
+-- GROUP BY b.machine_id;
+
+-- Self Join
+SELECT s.machine_id, round(avg(e.timestamp - s.timestamp)::numeric, 3) AS processing_time
+FROM activity s
+JOIN activity e 
+ON e.machine_id = s.machine_id
+AND e.process_id = s.process_id
+WHERE s.activity_type = 'start' AND e.activity_type = 'end'
+GROUP BY s.machine_id
