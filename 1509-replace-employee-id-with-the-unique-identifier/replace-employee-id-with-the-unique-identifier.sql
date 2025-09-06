410e72204668414bac72ed686228b0734cbecc57
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-06 16:59:50 +0530

    Time: 452 ms (71.31%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,4 @@
 SELECT COALESCE(EmployeeUNI.unique_id, NULL) AS unique_id, Employees.name
+-- COALESCE is unnessary, since joins by default gives NULL for empty values
 FROM Employees
 LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
