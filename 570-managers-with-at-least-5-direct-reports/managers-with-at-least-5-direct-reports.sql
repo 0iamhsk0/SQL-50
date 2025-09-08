0980d47a3a9b5bd86e9bc52969f8cd43e9140a9e
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-08 23:10:11 +0530

    Time: 293 ms (78.22%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,3 @@
--- -- Write your PostgreSQL query statement below
--- select name from employee where id in
--- (select managerId from employee group by managerId
--- having (count(distinct id)>=5))
-
 SELECT e1.name
 FROM Employee e1
 JOIN Employee e2 ON e1.id = e2.managerID
