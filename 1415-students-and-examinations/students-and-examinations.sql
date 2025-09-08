f0549a7bbdea78fe12909c764016fb5da9f9af22
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-08 22:43:02 +0530

    Time: 435 ms (34.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+FROM Students AS s
+CROSS JOIN Subjects AS sub
+LEFT JOIN Examinations AS e
+ON s.student_id = e.student_id 
+AND sub.subject_name = e.subject_name
+GROUP BY sub.subject_name, s.student_name, s.student_id
+ORDER BY student_id;
