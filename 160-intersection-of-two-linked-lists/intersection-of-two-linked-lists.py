c6ab91f9c7d76741bc481252bc17cbea2290dcde
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-11 20:05:06 +0530

    Time: 85 ms (57.96%) | Memory: 27.8 MB (70.05%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -5,28 +5,29 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        cntA = cntB = 0
-        currA = headA
-        currB = headB
-        while currA is not None:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:        # Count lengths
+        cntA, cntB = 0, 0
+        currA, currB = headA, headB
+        while currA:
             cntA += 1
             currA = currA.next
-        
-        while currB is not None:
+        while currB:
             cntB += 1
             currB = currB.next
 
-        if cntA > cntB:
-            for _ in range(cntA - cntB):
-                headA = headA.next
-        else:
-            for _ in range(cntB - cntA):
-                headB = headB.next
-            
-        while headA is not None and headB is not None:
+        # Align pointers
+        while cntA > cntB:
+            headA = headA.next
+            cntA -= 1
+        while cntB > cntA:
+            headB = headB.next
+            cntB -= 1
+
+        # Move together and check for intersection
+        while headA and headB:
             if headA is headB:
                 return headA
             headA = headA.next
             headB = headB.next
+        return None
         
