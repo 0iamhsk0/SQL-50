a45f18aff87fc7a5b23d10a7f52c43818dc1994b
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-09-17 18:20:35 +0530

    Time: 364 ms (65.61%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,7 +1,8 @@
 -- Write your PostgreSQL query statement below
 SELECT 
     activity_date AS day, COUNT(DISTINCT user_id) AS active_users  
-FROM Activity
+FROM 
+    Activity
 GROUP BY 
     activity_date
 HAVING
